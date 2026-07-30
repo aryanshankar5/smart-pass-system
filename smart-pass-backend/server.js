d7ce110cc7f5d199504f23b246fddf908a8a7e4b
Aryan Shankar <prem.shanker.2005@gmail.com>
2026-07-31 01:16:42 +0530

    Admin sidebar UI upgrade

--- smart-pass-backend/server.js
+++ smart-pass-backend/server.js
@@ -16,7 +16,6 @@ const corsOptions = {
     const allowedOrigins = [
       'http://localhost:5500',
       'http://127.0.0.1:5500',
-      'http://127.1.1.1:5500',
       'https://bitmesspass.netlify.app',
       'null'
     ];
